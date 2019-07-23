ee6ae94146919f5e256c6a2253ec572a1f60ea4f
Kaylee Asai <kayleeasai@Kaylees-MacBook-Pro.local>
2019-07-22 19:13:19 -0700

    rock_paper_scissors.py

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,46 +1,46 @@
->>> #HW4
-... 
->>> import random
->>> ComputerWins=0
->>> HumanWins=0
->>> while True:
-...     print('Make a move!(R/P/S):')
-...     Human=input()
-...     Computer=random.choice(['R','P','S'])
-...     if Human==Computer:
-...         print('Draw!')
-...     elif Human=='P':
-...         if Computer=='S':
-...             print('You chose '+Human+' and the computer chose '+Computer)
-...             print('Computer Wins!')
-...             ComputerWins+=1
-...         elif Computer=='R':
-...             print('You chose '+Human+' and the computer chose '+Computer)
-...             print('You Win!')
-...             HumanWins+=1
-...     elif Human=='S':
-...         if Computer=='R':
-...             print('You chose '+Human+' and the computer chose '+Computer)
-...             print('Computer Wins!')
-...             ComputerWins+=1
-...         elif Computer=='P':
-...             print('You chose '+Human+' and the computer chose '+Computer)
-...             print('You Win!')
-...             HumanWins+=1
-...     elif Human=='R':
-...         if Computer=='P':
-...             print('You chose '+Human+' and the computer chose '+Computer)
-...             print('Computer Wins!')
-...             ComputerWins+=1
-...         elif Computer=='S':
-...             print('You chose '+Human+' and the computer chose '+Computer)
-...             print('You Win!')
-...             HumanWins+=1
-...     elif Human=='sc':
-...         print('Human: '+str(HumanWins)+', Computer: '+str(ComputerWins))
-...     print('Do you want to play again? (y/n)')
-...     Play=input()
-...     if Play == 'n':
-...         print('Thanks, Bye!!!')
-...         break
+#HW4
+
+import random
+ComputerWins=0
+HumanWins=0
+while True:
+     print('Make a move!(R/P/S):')
+     Human=input()
+     Computer=random.choice(['R','P','S'])
+     if Human==Computer:
+         print('Draw!')
+     elif Human=='P':
+         if Computer=='S':
+             print('You chose '+Human+' and the computer chose '+Computer)
+             print('Computer Wins!')
+             ComputerWins+=1
+         elif Computer=='R':
+             print('You chose '+Human+' and the computer chose '+Computer)
+             print('You Win!')
+             HumanWins+=1
+     elif Human=='S':
+         if Computer=='R':
+             print('You chose '+Human+' and the computer chose '+Computer)
+             print('Computer Wins!')
+             ComputerWins+=1
+         elif Computer=='P':
+             print('You chose '+Human+' and the computer chose '+Computer)
+             print('You Win!')
+             HumanWins+=1
+     elif Human=='R':
+         if Computer=='P':
+             print('You chose '+Human+' and the computer chose '+Computer)
+             print('Computer Wins!')
+             ComputerWins+=1
+         elif Computer=='S':
+             print('You chose '+Human+' and the computer chose '+Computer)
+             print('You Win!')
+             HumanWins+=1
+     elif Human=='sc':
+         print('Human: '+str(HumanWins)+', Computer: '+str(ComputerWins))
+     print('Do you want to play again? (y/n)')
+     Play=input()
+     if Play == 'n':
+         print('Thanks, Bye!!!')
+         break
 
